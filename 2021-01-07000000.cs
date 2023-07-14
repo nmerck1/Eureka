5c1ce390c95cf33462f760791dce3dcaf44501ed
Nathaniel Merck <nmerck1@tctc.edu>
2023-07-14 06:19:59 -0400

    Commit on 2021-01-07 00:00:00 by nmerck1

---
+++ 2021-01-07000000.cs
@@ -0,0 +1,8 @@
+List<string> names = new List<string> { "Alice", "Bob", "Charlie" };
+public static int Add(int a, int b) { return a + b; }
+DateTime now = DateTime.Now;
+int[] numbers = { 1, 2, 3, 4, 5 };
+public class MyClass { public int MyProperty { get; set; } }
+for (int i = 0; i < 10; i++) { Console.WriteLine(i); }
+string name = "John";
+Console.WriteLine("Hello, World!");
